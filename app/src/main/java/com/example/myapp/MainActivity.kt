c06d9dcbe81a35f9f9e7f809afea1e5c690ae6dd
Yingzhiemily <emilyyingzhi@gmail.com>
2023-02-01 16:43:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapp/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.example.myapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.myapp.ui.theme.MyAppTheme
+import androidx.compose.ui.unit.dp
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyAppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    MyApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun MyApp() {
+    val appModifier = Modifier.fillMaxWidth().padding(8.dp)
+
+    Column(
+        verticalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        Text(
+            text = "My title",
+            style = MaterialTheme.typography.h5,
+            modifier = appModifier
+        )
+        OutlinedTextField(
+            value = "" ,
+            onValueChange = { /*TODO*/ },
+            modifier = appModifier
+        )
+        Button(
+            onClick = { /*TODO*/ },
+            modifier = appModifier
+        ) {
+            Text(text = "Submit")
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MyAppTheme {
+        MyApp()
+    }
+}
